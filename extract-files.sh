7eb284ce6acaa6463d8ed9428f77e03d5c249932
Michael Bestas <mkbestas@gmail.com>
2024-09-11 00:04:13 +0300

    stone: Shim widevine with libcrypto_shim
    The following change
    https://github.com/google/boringssl/commit/e202e51cb0912f36dafbd2e67cf04d6ec82f3180
    removed symbols that our libs require.
    
    Change-Id: I1325c34ecec3d339dd68c665010ecb36fa2e6a29

--- extract-files.sh
+++ extract-files.sh
@@ -86,6 +86,10 @@ function blob_fixup() {
             [ "$2" = "" ] && return 0
             sed -i "s/libhidltransport.so/libhidlbase_shim.so/" "${2}"
             ;;
+        vendor/lib64/libwvhidl.so|vendor/lib64/mediadrm/libwvdrmengine.so)
+            [ "$2" = "" ] && return 0
+            grep -q "libcrypto_shim.so" "${2}" || "${PATCHELF}" --add-needed "libcrypto_shim.so" "${2}"
+            ;;
         vendor/lib64/camera/components/com.qti.node.mialgocontrol.so)
             [ "$2" = "" ] && return 0
             grep -q "libpiex_shim.so" "${2}" || "${PATCHELF}" --add-needed "libpiex_shim.so" "${2}"

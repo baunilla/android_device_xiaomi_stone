841f60ac7d7c68523602bcb3ac4cf629f24de317
LuK1337 <priv.luk@gmail.com>
2024-10-12 17:42:02 +0200

    stone: Shim WFD with libgui_shim/libinput_shim
    Change-Id: I42bf06bc6b19942303031ef0147dafa6df127fe5

--- extract-files.sh
+++ extract-files.sh
@@ -61,13 +61,17 @@ fi
 
 function blob_fixup() {
     case "${1}" in
+        system_ext/lib*/libwfdmmsrc_system.so)
+            [ "$2" = "" ] && return 0
+            grep -q "libgui_shim.so" "${2}" || "${PATCHELF}" --add-needed "libgui_shim.so" "${2}"
+            ;;
         system_ext/lib*/libwfdservice.so)
             [ "$2" = "" ] && return 0
             sed -i "s/android.media.audio.common.types-V2-cpp.so/android.media.audio.common.types-V3-cpp.so/" "${2}"
             ;;
         system_ext/lib64/libwfdnative.so)
             [ "$2" = "" ] && return 0
-            ${PATCHELF} --remove-needed "android.hidl.base@1.0.so" "${2}"
+            sed -i "s/android.hidl.base@1.0.so/libinput_shim.so\x00\x00\x00\x00\x00\x00\x00\x00/" "${2}"
             ;;
         vendor/etc/camera/camxoverridesettings.txt)
             [ "$2" = "" ] && return 0
